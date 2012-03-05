c5c9855b536788fc007d9d0659360b9e218c2f48
ryan spangler <ryan.spangler@gmail.com>
2012-03-05 14:51:36 -0800

    added some pure ratio tonalities

--- src/prism/tonality.clj
+++ src/prism/tonality.clj
@@ -19,10 +19,14 @@
   (let [step (Math/pow 2 (/ 1 n))]
     (take n (iterate #(* step %) 1.0))))
 
-;; Nineteen tone equal temperament -------------
-;;   =~ (1/1 26/25 14/13 10/9 8/7 6/5 5/4 9/7 4/3 7/5 10/7 3/2 14/9 8/5 5/3 7/4 9/5 13/7 25/13)
-(def nineteen
-  (tonality (equal-temperament 19) 200.0 48))
+(def pure-twelve
+  [1/1 16/15 9/8 6/5 5/4 4/3 11/8 3/2 8/5 5/3 16/9 15/8])
+
+(def dark-twelve
+  [1/1 17/16 10/9 7/6 11/9 9/7 13/9 3/2 11/7 13/8 7/4 13/7])
+
+(def pure-nineteen
+  [1/1 26/25 14/13 10/9 8/7 6/5 5/4 9/7 4/3 7/5 10/7 3/2 14/9 8/5 5/3 7/4 9/5 13/7 25/13])
 
 (defn- find-octave
   [note order]
@@ -45,3 +49,9 @@
           power (* fundamental (Math/pow 2 octave))
           frequency (* power ratio)]
       frequency)))
+
+;; Nineteen tone equal temperament -------------
+;;   =~ (1/1 26/25 14/13 10/9 8/7 6/5 5/4 9/7 4/3 7/5 10/7 3/2 14/9 8/5 5/3 7/4 9/5 13/7 25/13)
+(def nineteen
+  (tonality (equal-temperament 19) 200.0 48))
+
