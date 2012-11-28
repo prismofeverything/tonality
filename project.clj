a16c11dca7e5667099db9120a3f33095de71364c
ryan spangler <ryan.spangler@gmail.com>
2012-11-27 18:00:36 -0800

    updated for clojars release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject prism "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject prism "0.0.1"
+  :description "Tonalities for mapping tones to frequencies"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [overtone "0.6.0"]]
+                 [overtone "0.7.1"]]
   :dev-dependencies [[swank-clojure "1.4.2"]]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
