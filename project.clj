e6070dc761d4974bb17571e1240d9489ef0efa71
ryan spangler <ryan.spangler@gmail.com>
2013-10-20 17:44:47 -0700

    clojure version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject tonality "0.0.3"
+(defproject tonality "0.0.4"
   :description "Tonalities for mapping tones to frequencies"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [overtone "0.8.1"]]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
